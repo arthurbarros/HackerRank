73cc1c9e03631acb689edf8985c71a9ad10bc299
Arthur Barros <arthbarros@gmail.com>
2020-04-17 21:01:02 -0300

    Create Birthday Cake Candles.py

---
+++ Algorithms/Birthday Cake Candles.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    ar.sort(reverse=True)
+    tallest = ar[0]
+    return sum(1 for el in ar if el == tallest)
+
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
